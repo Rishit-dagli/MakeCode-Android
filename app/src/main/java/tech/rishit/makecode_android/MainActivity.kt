17d05afeab88869ccf9b85beee9f5de727575e98
Rishit Dagli <rishit.dagli@gmail.com>
2020-05-21 17:21:01 +0000

    Added GitHub button

--- app/src/main/java/tech/rishit/makecode_android/MainActivity.kt
+++ app/src/main/java/tech/rishit/makecode_android/MainActivity.kt
@@ -1,20 +1,25 @@
 package tech.rishit.makecode_android
 
 import android.annotation.SuppressLint
+import android.graphics.Color
 import android.os.Bundle
 import android.view.View
 import android.webkit.WebView
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 import com.mahfa.dnswitch.DayNightSwitch
+import org.w3c.dom.Text
 
 class MainActivity : AppCompatActivity() {
     lateinit var dayNightSwitch: DayNightSwitch
     lateinit var background_view: View
     lateinit var textView: TextView
     lateinit var myWebView: WebView
+    lateinit var github: FloatingActionButton
+    lateinit var project_name: TextView
 
 
     @SuppressLint("SetJavaScriptEnabled")
@@ -25,6 +30,9 @@ class MainActivity : AppCompatActivity() {
         dayNightSwitch = findViewById<View>(R.id.dayNight) as DayNightSwitch
         background_view = findViewById(R.id.background_view)
         myWebView = findViewById(R.id.webview)
+        github = findViewById(R.id.github)
+        project_name = findViewById(R.id.project_name)
+
         myWebView.settings.javaScriptEnabled = true
 
         val projectUrl: String = getString(R.string.makecode_project)
@@ -37,6 +45,7 @@ class MainActivity : AppCompatActivity() {
                 this.background_view.alpha = 1f
             } else {
                 Toast.makeText(this@MainActivity, "Day Mode!", Toast.LENGTH_SHORT).show()
+
                 this.background_view.alpha = 0f
             }
         }
