df96f0e860f940beba0442e162aeeed09b2de528
Rishit Dagli <rishit.dagli@gmail.com>
2020-05-22 03:17:30 +0000

    Added download image class

--- app/src/main/java/tech/rishit/makecode_android/credits.kt
+++ app/src/main/java/tech/rishit/makecode_android/credits.kt
@@ -2,11 +2,39 @@ package tech.rishit.makecode_android
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.os.AsyncTask
+import android.util.Log
+import android.widget.ImageView
+
 
 class credits : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_credits)
+    }
+}
+
+private class DownloadImageTask(internal var bmImage: ImageView) :
+    AsyncTask<String, Void, Bitmap>() {
+
+    override fun doInBackground(vararg urls: String): Bitmap? {
+        val urldisplay = urls[0]
+        var mIcon11: Bitmap? = null
+        try {
+            val `in` = java.net.URL(urldisplay).openStream()
+            mIcon11 = BitmapFactory.decodeStream(`in`)
+        } catch (e: Exception) {
+            Log.e("Error", e.message)
+            e.printStackTrace()
+        }
+
+        return mIcon11
+    }
+
+    override fun onPostExecute(result: Bitmap) {
+        bmImage.setImageBitmap(result)
     }
 }
