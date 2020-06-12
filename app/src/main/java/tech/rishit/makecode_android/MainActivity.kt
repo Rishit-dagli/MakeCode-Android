72ea68681dae69673bfbf6c34cf20386aba3211c
Rishit Dagli <rishit.dagli@gmail.com>
2020-06-12 15:04:56 +0000

    Fixed typo

--- app/src/main/java/tech/rishit/makecode_android/MainActivity.kt
+++ app/src/main/java/tech/rishit/makecode_android/MainActivity.kt
@@ -3,7 +3,7 @@
  * Copyright (c) 2020.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
+ * you may not use this file except ̥in compliance with the License.
  *
  * You may obtain a copy of the License at-
  *
